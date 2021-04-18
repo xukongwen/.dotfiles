9261a7d3cc367746f541db0c0a07c5a20a5196bd
xukongwen <oabdao@gmail.com>
2021-04-18 09:56:34 +0800

    hi

---
+++ qtile/autostart.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash 
+
+#festival --tts $HOME/.config/qtile/welcome_msg &
+#lxsession &
+#picom &
+feh --bg-scale ~/wallpapers/w1.jpg &
+
+#nitrogen --restore &
+/usr/bin/emacs --daemon &
+#volumeicon &
+#nm-applet &
+fcitx5 &
