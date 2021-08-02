37d464b87560b6abe682578146f549f7dc9d0994
xukongwen <oabdao@gmail.com>
2021-08-02 14:55:32 +0800

    hi

---
+++ emacs/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir ~/git/
+cd git
+git clone https://github.com/manateelazycat/auto-save.git
+cd
+cp ~/.dotfiles/emacs/init.el ~/.emacs.d/
+cp -rf ~/.dotfiles/emacs/nano ~/.emacs.d/
+
+
